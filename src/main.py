3adf1baa0e99d4212706b71fd0fe8b8e15619a06
MrNil <poria2100@gmail.com>
2021-06-05 11:57:47 +0430

    Finish the project

--- src/main.py
+++ src/main.py
@@ -9,21 +9,24 @@ line_to_search = 0
 chars_to_press = ""
 
 # Copy all the html elements to a file and source it here
-with open("../../ls/src/elements", 'r') as elements:
+with open("[file]", 'r') as elements:
     for line_i, line  in enumerate(elements, 1):
+        # Find the line containing "<letter>"
         if(regex.search(line)):
             splited = line.split("<letter>")
             char = 1
             for i in splited:
                 # pyautogui.press(splited[char][0], interval=0.0000002)
                 try:
+                    # Add the chars
                     chars_to_press = chars_to_press + splited[char][0]
                 except IndexError:
                     break
+                # if reached to the end of word, add space
                 if len(splited[char]) > 20:
                     # pyautogui.press('space', interval=0.0002)
                     chars_to_press += ' '
                 char += 1
 
-
+# Write the paragraph
 pyautogui.write(chars_to_press, interval=0.025)
