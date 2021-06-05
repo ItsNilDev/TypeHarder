8e2af152b5038781246e813398c7ca244085ae67
MrNil <poria2100@gmail.com>
2021-06-05 11:51:12 +0430

    Clean up

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import re
 import pyautogui
+import time
 
+# wait for some seconds
+time.sleep(5)
 regex = re.compile("<letter>(.*)<letter>")
 line_to_search = 0
 chars_to_press = ""
 
+# Copy all the html elements to a file and source it here
 with open("../../ls/src/elements", 'r') as elements:
     for line_i, line  in enumerate(elements, 1):
         if(regex.search(line)):
@@ -12,13 +16,14 @@ with open("../../ls/src/elements", 'r') as elements:
             char = 1
             for i in splited:
                 # pyautogui.press(splited[char][0], interval=0.0000002)
-                if char == 446:
+                try:
+                    chars_to_press = chars_to_press + splited[char][0]
+                except IndexError:
                     break
-                chars_to_press = chars_to_press + splited[char][0]
                 if len(splited[char]) > 20:
                     # pyautogui.press('space', interval=0.0002)
                     chars_to_press += ' '
                 char += 1
 
 
-pyautogui.write(chars_to_press, interval=0.000001)
+pyautogui.write(chars_to_press, interval=0.025)
